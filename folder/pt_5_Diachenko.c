5b1b37eeabfaa79084153cc0152325c1cfb74e98
karadin25 <98397908+karadin25@users.noreply.github.com>
2024-07-05 12:09:27 +0300

    Update pt_5_Diachenko.c

--- folder/pt_5_Diachenko.c
+++ folder/pt_5_Diachenko.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+#define MOD 12345
+
+int main() {
+    int n;
+    printf("Input the length of the sequence n:");
+    scanf("%d", &n);
+    if (n < 2 || n >= 10000)
+    {
+        printf("The length of the sequence must be in the range 2 <= n < 10000\n");
+        return 1;
+    }
+
+    int a[n+1], b[n+1], c[n+1];
+
+    a[1] = 1;
+    b[1] = 1;
+    c[1] = 0;
+
+    for (int i = 2; i <= n; i++) {
+        a[i] = (a[i-1] + b[i-1] + c[i-1]) % MOD;
+        b[i] = a[i-1];
+        c[i] = b[i-1];
+    }
+
+    int result = (a[n] + b[n] + c[n]) % MOD;
+    printf("Number of searched sequences: %d\n", result);
+
+    return 0;
+}
 
